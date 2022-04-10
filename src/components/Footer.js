b890cb75d53a3c3f8a3e8a2a4067c3752f0e7273
Rahul Choudhary <rc9782276591@gmail.com>
2022-04-10 22:56:41 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+
+import { Link } from "react-router-dom"
+
+const Footer = () => {
+  return (
+    <footer>
+    <p>Copyright &copy; 2022</p>
+    <Link to="/about">about</Link>
+    </footer>
+  )
+}
+
+export default Footer
